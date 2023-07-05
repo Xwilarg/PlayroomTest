862b676258873d6a9c40d84f678f8a2201011e24
Christian Chaux <xwilarg@protonmail.com>
2023-07-05 11:44:45 +0000

    Add base playroom code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import { PerspectiveCamera, Scene, WebGLRenderer } from "three"
+import { onPlayerJoin, insertCoin, isHost, myPlayer } from "playroomkit";
 
-const scene = new Scene()
+const scene = new Scene();
 const camera = new PerspectiveCamera(45, window.innerWidth / window.innerWidth, 1, 1000);
 
-const renderer = new WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
 renderer.setAnimationLoop(() => {
     renderer.render(scene, camera);
 })
+
+onPlayerJoin((state) => {
+    console.log(`Player joined: {state}`)
+});
+insertCoin();
